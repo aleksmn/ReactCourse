4e82fcaef856d03c0d3dbe3c0aff672ca5eeb933
aleksmn <al-mn@yandex.ru>
2022-10-11 10:45:05 +0300

    add state

--- 02-counter-app/src/components/counter.jsx
+++ 02-counter-app/src/components/counter.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+
+    state = {
+        count: 0,
+        imageUrl: 'https://source.unsplash.com/200x200/?computer',
+    }
+
     render() {
         return (
             <React.Fragment>
+                <img src={this.state.imageUrl} alt="" />
                 <h1>Привет, всем!</h1>
+                <span>{this.formatCount()}</span>
                 <button>Increment</button>
             </React.Fragment>
         );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
 
