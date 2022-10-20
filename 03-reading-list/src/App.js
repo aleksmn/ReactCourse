04d02523994ae866f225059b6e45b6f382f43f68
aleksmn <al-mn@yandex.ru>
2022-10-20 11:54:28 +0300

    init

---
+++ 03-reading-list/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+
+    <main className='container py-4 py-lg-5'>
+        <h1>Get started with Bootstrap</h1>
+      </main>
+  );
+}
+
+export default App;
