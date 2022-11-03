d70a0561c21a382e7fe87a6f8d0294d960e21643
aleksmn <al-mn@yandex.ru>
2022-11-03 19:23:44 +0300

    fix

--- 03-reading-list/src/components/books.jsx
+++ 03-reading-list/src/components/books.jsx
@@ -1,8 +1,10 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { getBooks } from '../services/bookService';
 
 // imrc - shortcut create react component
 // cc - create class
+
+
 
 class Books extends Component {
   state = {
