339f5b9f01621346ae599b8aef04099d4b84dae0
aleksmn <al-mn@yandex.ru>
2022-12-13 14:21:05 +0300

    init

---
+++ 05-reading-list/src/components/books.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { getBooks } from '../services/bookService';
+import Like from './common/like';
+
+// imrc - shortcut create react component
+// cc - create class
+
+
+
+class Books extends Component {
+  state = {
+    books: getBooks()
+  };
+
+  handleDelete = (book) => {
+    console.log(book)
+    const books = this.state.books.filter(b => b._id !== book._id)
+    this.setState({ books: books })
+  }
+
+  handleLike = book => {
+    // console.log('Лайк!', book)
+
+    const books = [...this.state.books];
+    const index = books.indexOf(book);
+    books[index] = {...books[index]};
+    books[index].liked = !books[index].liked;
+    this.setState({ books })
+    
+  }
+
+  render() {
+    if (this.state.books.length === 0) return <p>Здесь нет ни одной книги :(</p>
+
+    return (
+      <React.Fragment>
+        <p>В списке книг: {this.state.books.length}</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Название</th>
+              <th>Автор</th>
+              <th>Жанр</th>
+              <th>Стр.</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.books.map(book => (
+              <tr key={book._id}>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>{book.genre.name}</td>
+                <td>{book.pages}</td>
+                <td>
+                  <Like liked={book.liked} onLikeToggle={() => this.handleLike(book)}/>
+                </td>
+                <td><button onClick={() => this.handleDelete(book)} className="btn btn-danger btn-sm">Удалить</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>);
+  }
+}
+
+export default Books;
+
+
