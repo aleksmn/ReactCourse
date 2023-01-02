36ba22bb85d87b0e1da5d937af241e43188b2cdb
aleksmn <al-mn@yandex.ru>
2023-01-02 16:10:04 +0300

    del

--- 05-reading-list-pages/src/services/genreService.js
+++
@@ -1,9 +0,0 @@
-export const genres = [
-  { _id: "5b21ca3eeb7f6fbccd471818", name: "Классика" },
-  { _id: "5b21ca3eeb7f6fbccd471814", name: "Фантастика" },
-  { _id: "5b21ca3eeb7f6fbccd471820", name: "Приключения" }
-];
-
-export function getGenres() {
-  return genres.filter(g => g);
-}
