31632dd01e871c9d048d8e54baff41208eef39ba
aleksmn <al-mn@yandex.ru>
2023-01-02 14:05:58 +0300

    init

---
+++ 07-reading-list-sorting/src/components/common/like.jsx
@@ -0,0 +1,17 @@
+
+// sfc
+
+// Stateless Function Component
+
+
+const Like = (props) => {
+
+    let classes = "fa fa-heart";
+    if (!props.liked) classes += "-o";
+    
+    return <i onClick={props.onLikeToggle} style={{ cursor: "pointer" }} className={classes} aria-hidden="true"></i>; 
+
+}
+ 
+export default Like;
+
