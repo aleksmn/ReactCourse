c9675d672269c67237f16c08f5544ee1b6e89430
aleksmn <al-mn@yandex.ru>
2023-01-06 15:25:26 +0300

    fix styles

--- 05-reading-list-app/src/index.js
+++ 05-reading-list-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import './css/bootstrap-cyborg.min.css'
+import './css/bootstrap-superhero.min.css'
 import './css/index.css'
 import 'font-awesome/css/font-awesome.css';
 
