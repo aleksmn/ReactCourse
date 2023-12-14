b363f31db163b2df940d73161078ea1296c18d2c
aleksmn <al-mn@yandex.ru>
2023-12-14 17:57:51 +0300

    fix

--- 02-counter-app/src/components/counter.jsx
+++ 02-counter-app/src/components/counter.jsx
@@ -12,12 +12,17 @@ class Counter extends Component {
         this.setState({ count: this.state.count + 1 })
     }
 
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
     render() {
         let classes = this.state.count === 0 ? "badge m-2 text-bg-warning" : "badge m-2 text-bg-primary"
 
         return (
             <React.Fragment>
-                <span className={classes}>{this.state.count}</span>
+                <span className={classes}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Клик</button>
                 <ul>
                     { this.state.tags.map( tag => <li key={tag} >{tag}</li>) }
@@ -27,20 +32,6 @@ class Counter extends Component {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-    // formatCount() {
-    //     const { count } = this.state;
-    //     return count === 0 ? 'Zero' : count;
-    // }
 }
 
 export default Counter;
